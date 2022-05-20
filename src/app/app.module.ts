3e3c377976c84380b0d01acbf625364c176703a9
Matt Crecraft <mcrecraft@gmail.com>
2022-05-19 23:33:05 -0500

    Completed navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { LogoComponent } from './logo/logo.component';
+import { LinksComponent } from './links/links.component';
+import { AvatarComponent } from './avatar/avatar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
+    LinksComponent,
+    AvatarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
--- src/hero.ts
+++ src/hero.ts
@@ -4,4 +4,9 @@ export const heroContent = {
     imageUrl: '/assets/rex-goliath-logo.png',
     description: `America's most unhinged rooster is out of retirement, and this time? He's mad
   as hell.  Experience the unapologetic flavor burst of Rex Goliath.`,
+
+    heroImage: {
+        url: '/assets/wine-hero.webp',
+        alt: 'Wine bottles',
+    }
 }
